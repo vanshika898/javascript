104029ffbd28c62b07ac56af35ecc31adf3d5800
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-11 01:19:27 +0530

    day2

---
+++ concat/comparision/Nullish.js
@@ -0,0 +1,17 @@
+let c = "prakash";
+console.log(c ?? "hidden");
+
+let k ;
+console.log(k ?? "hidden");
+
+
+let s = "";
+console.log(s??"hidden");
+
+
+let firstName = "";
+console.log(firstName || "Hidden Geek");  
+
+setTimeout(function() {
+    console.log("This is a callback function!");
+}, 1000);
---
+++ concat/comparision/q.js
@@ -0,0 +1,13 @@
+console.log(50 > 30);  // true
+console.log(50 < 30);  // false
+console.log(30 >= 30); // true
+console.log(50 <= 40); //false
+console.log(50 == 40); // false
+
+
+
+console.log("apple" > "banana");  // false
+console.log("glowing" > "glow");  // true
+
+console.log("2" > 1);   // true
+console.log("01" == 1); // true
