d1394856d7405ec6e9d48511621f8fe57c3c4adb
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-21 21:18:29 +0530

    day3

---
+++ StringMethods.java/charAtMethod.js
@@ -0,0 +1,9 @@
+let message = "my name is vanshika nigam";
+
+let index =8;
+let char = message.charAt(index);
+console.log();
+
+let asii = message.charCodeAt(index);
+
+console.log(`so the ascii code of ${char} is ${asii}`);
---
+++ StringMethods.java/indexMethod.js
@@ -0,0 +1,8 @@
+let message = 'vanshika nigam';
+
+var lastIndex = message.lastIndexOf('a');
+var index = message.indexOf('a');
+console.log(lastIndex);
+
+
+console.log(`first index of a is ${index} and last index of char a is ${lastIndex}`)
