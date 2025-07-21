742fecd1f5f3323470ffdcac64f3c2673d42f1cb
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-21 21:35:14 +0530

    day3

---
+++ StringMethods.java/subString.js
@@ -0,0 +1,11 @@
+
+var string = "geeksforgeeks";
+a = string.substring(0, 4)
+b = string.substring(1, 6)
+c = string.substring(5)
+d = string.substring(0)
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
