a20c89d1a753d195c53116a8b26268a9ba248724
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-25 23:59:35 +0530

    day6

---
+++ optionalChaning/Destructing_object.js
@@ -0,0 +1,9 @@
+const obj = {
+    name: 'Prakash',
+    address: {
+        street: '123 Main St',
+        city: 'Mumbai',
+        state: 'Maharashtra'
+    },
+    courses: ['JavaScript', 'React', 'Node.js']
+};
