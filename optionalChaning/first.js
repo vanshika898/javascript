0c9fe9b86c969e02d4cd8131df1a6aea75bbff8a
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-25 23:45:55 +0530

    day6

---
+++ optionalChaning/first.js
@@ -0,0 +1,12 @@
+const obj = {
+    name :"vanshika",
+    age : 21,
+    address: {
+     city : "Ujjain",
+     State  : "Madhya Pradesh",
+     country : "India"
+    }
+
+}
+console.log(obj.name);
+console.log(obj.address.State)
