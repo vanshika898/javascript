5285166befda0f6b604416bb83fe6358260a4739
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-26 23:51:07 +0530

    es module

--- Modules/index.js
+++ Modules/index.js
@@ -1,5 +1,11 @@
 // const {print,greet} = require("./utils"); // common js
 import {greet,print} from './utils.js';
 
+
+const isReuiredMath = true;
+if(isReuiredMath){
+    const {add} = await import ('./math.js');
+    console.log(add(2,3));
+}
 print("any");
 greet("vanshika");
---
+++ Modules/math.js
@@ -0,0 +1,5 @@
+// dynamic importing
+
+export function add(value1,value2){
+    return value1+value2;
+}
