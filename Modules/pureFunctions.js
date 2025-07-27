fd8eb5094d9fbe9d73a065d5e3e8f7104b545b61
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-27 18:51:44 +0530

    impure function becomes pure

--- Modules/pureFunctions.js
+++ Modules/pureFunctions.js
@@ -3,3 +3,30 @@ function add (num){
 }
 
 console.log(add(2));
+
+// t takes an input (number).
+// It returns a value (number * 2).
+// It doesn't modify any external variables.
+// The result is always the same for the same input.
+
+
+
+function appendArr(arr){ ///this is impure function becaues it mutate original array
+    arr.push(5,6);
+    return arr;
+}
+
+let num = [1,2,3,4];
+console.log(appendArr(num));
+
+
+
+//making it pure
+
+function appendit(num){
+    let newArr = [...num,5,6];
+    return newArr;
+}
+
+let num1 = [1,2,3];
+console.log(appendit(num));
