afa2128b54f1d0f4eb460149e76b8dc5d1148a22
vanshika898 <vanshikanigam8989@gmail.com>
2025-07-30 22:13:57 +0530

    map ,filter

---
+++ HigherOrderFunctions/map.js
@@ -0,0 +1,13 @@
+const a = [1,2,3,4];
+// const b = a.map((i)=>i*3);
+
+const b = a.map(triple);
+console.log(b);
+
+const k = b.filter((i)=>i%2 === 0);
+console.log(k);
+
+function triple(n){
+return n*3;
+}
+
